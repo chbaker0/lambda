d33f2ebcde9040f3ae574a55d755b6f84d2bf439
Collin Baker <chbaker0@gmail.com>
2016-09-04 13:30:52 -0400

    Abstract syntax tree for evaluation

--- eval/Eval.hpp
+++ eval/Eval.hpp
@@ -1,15 +1,74 @@
 #ifndef EVAL_HPP_INCLUDED
 #define EVAL_HPP_INCLUDED
 
+#include <memory>
+#include <string>
+
 namespace eval
 {
 
-namespace tree
+namespace ast
 {
 
+class TermVisitor;
 
+class Term
+{
+public:
 
-} // tree
+    virtual ~Term() {}
+
+    virtual void applyVisitor(TermVisitor&) const = 0;
+};
+
+class Abstraction : public Term
+{
+public:
+
+    std::string argumentTag;
+    std::unique_ptr<Term> body;
+
+    virtual void applyVisitor(TermVisitor&) const override;
+};
+
+class Application : public Term
+{
+public:
+
+    std::unique_ptr<Term> left, right;
+
+    virtual void applyVisitor(TermVisitor&) const override;
+};
+
+class BoundVariable : public Term
+{
+public:
+
+    unsigned int index;
+
+    virtual void applyVisitor(TermVisitor&) const override;
+};
+
+class FreeVariable : public Term
+{
+public:
+
+    std::string name;
+
+    virtual void applyVisitor(TermVisitor&) const override;
+};
+
+class TermVisitor
+{
+public:
+
+    virtual void acceptTerm(const Abstraction&) = 0;
+    virtual void acceptTerm(const Application&) = 0;
+    virtual void acceptTerm(const BoundVariable&) = 0;
+    virtual void acceptTerm(const FreeVariable&) = 0;
+};
+
+} // ast
 
 } // eval
 
