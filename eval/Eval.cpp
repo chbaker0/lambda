17b78b72a12a377278cbee399a4460e11f35a54c
Collin Baker <chbaker0@gmail.com>
2016-09-05 15:57:04 -0400

    Perform resolution of bound variables

--- eval/Eval.cpp
+++ eval/Eval.cpp
@@ -114,13 +114,106 @@ public:
     std::unique_ptr<ast::Term> result;
 };
 
+class NameResolver : public ast::TermVisitor
+{
+public:
+
+    NameResolver(std::map<std::string, unsigned int>& symbolTable_in, unsigned int depth_in = 0)
+        : symbolTable(symbolTable_in), depth(depth_in) {}
+
+    virtual void acceptTerm(const ast::Abstraction& term) override
+    {
+        std::string argName = term.argumentName;
+
+        bool saved = false;
+        unsigned int savedIndex = 0;
+
+        auto it = symbolTable.find(argName);
+        if (it != symbolTable.end())
+        {
+            saved = true;
+            savedIndex = it->second;
+            it->second = depth;
+        }
+        else
+        {
+            symbolTable[argName] = depth;
+        }
+
+        NameResolver bodyVisitor(symbolTable, depth + 1);
+        term.body->applyVisitor(bodyVisitor);
+
+        std::unique_ptr<ast::Abstraction> resolvedTerm(new ast::Abstraction);
+        resolvedTerm->argumentName = argName;
+        resolvedTerm->body = std::move(bodyVisitor.result);
+        result = std::move(resolvedTerm);
+
+        if (saved)
+        {
+            symbolTable[argName] = savedIndex;
+        }
+        else
+        {
+            symbolTable.erase(argName);
+        }
+    }
+
+    virtual void acceptTerm(const ast::Application& term) override
+    {
+        std::unique_ptr<ast::Application> resolvedTerm(new ast::Application);
+
+        NameResolver visitor(symbolTable, depth);
+        term.left->applyVisitor(visitor);
+        resolvedTerm->left = std::move(visitor.result);
+        term.right->applyVisitor(visitor);
+        resolvedTerm->right = std::move(visitor.result);
+
+        result = std::move(resolvedTerm);
+    }
+
+    virtual void acceptTerm(const ast::FreeVariable& term) override
+    {
+        auto it = symbolTable.find(term.name);
+        if (it != symbolTable.end())
+        {
+            std::unique_ptr<ast::BoundVariable> resolvedTerm(new ast::BoundVariable);
+            resolvedTerm->index = depth - it->second;
+            result = std::move(resolvedTerm);
+        }
+        else
+        {
+            std::unique_ptr<ast::FreeVariable> unresolvedTerm(new ast::FreeVariable);
+            unresolvedTerm->name = term.name;
+            result = std::move(unresolvedTerm);
+        }
+    }
+
+    virtual void acceptTerm(const ast::BoundVariable& term) override
+    {
+        std::unique_ptr<ast::BoundVariable> duplicate(new ast::BoundVariable);
+        duplicate->index = term.index;
+        result = std::move(duplicate);
+    }
+
+    std::map<std::string, unsigned int>& symbolTable;
+    unsigned int depth;
+    std::unique_ptr<ast::Term> result;
+};
+
 } // anonymous
 
 std::unique_ptr<ast::Term> convertParseTree(const parse::tree::Term& parseTree)
 {
     ParseTreeConverter parseTreeConverter;
     parseTree.applyVisitor(parseTreeConverter);
-    return std::move(parseTreeConverter.result);
+
+    std::unique_ptr<ast::Term> unresolvedTree = std::move(parseTreeConverter.result);
+
+    std::map<std::string, unsigned int> symbolTable;
+    NameResolver nameResolver(symbolTable);
+    unresolvedTree->applyVisitor(nameResolver);
+
+    return std::move(nameResolver.result);
 }
 
 } // eval
